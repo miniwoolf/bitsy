2e58496c8c978cf03bb9a7729068dd8edf428d02
Adam Le Doux <adamrossledoux@gmail.com>
2020-10-10 14:12:47 -0700

    start to make tables editable
    todo: make the parameter type editable

--- editor/script/dialog_editor/table_editor.js
+++ editor/script/dialog_editor/table_editor.js
@@ -14,17 +14,39 @@ function TableEditor(expression, parentEditor) {
 	inputDescription.innerText = "make table containing entries:";
 	div.appendChild(inputDescription);
 
+	var entryEditors = [];
+
 	// todo : validate that input is correct?
 	for (var i = 1; i < expression.list.length; i += 2) {
-		var entryEditor = new TableEntryEditor(expression.list[i], expression.list[i + 1], this);
-		div.appendChild(entryEditor.GetElement());
+		var editor = new TableEntryEditor(expression.list[i], expression.list[i + 1], this);
+		entryEditors.push(editor);
+		div.appendChild(editor.GetElement());
 	}
 
 	this.GetElement = function() {
 		return div;
 	}
 
-	AddSelectionBehavior(this);
+	this.GetExpressionList = function() {
+		return [expression];
+	}
+
+	this.NotifyUpdate = function() {
+		parentEditor.NotifyUpdate();
+	}
+
+	AddSelectionBehavior(
+		this,
+		function() {
+			for (var i = 0; i < entryEditors.length; i++) {
+				entryEditors[i].Select();
+			}
+		},
+		function() {
+			for (var i = 0; i < entryEditors.length; i++) {
+				entryEditors[i].Deselect();
+			}
+		});
 }
 
 // todo : needs style so it's ok to have inline math expressions inside these
@@ -45,5 +67,23 @@ function TableEntryEditor(nameExpression, valueExpression, parentEditor) {
 
 	this.GetElement = function() {
 		return div;
+	}
+
+	this.GetExpressionList = function() {
+		return [nameExpression, valueExpression];
+	}
+
+	this.NotifyUpdate = function() {
+		parentEditor.NotifyUpdate();
+	}
+
+	this.Select = function() {
+		nameEditor.Select();
+		valueEditor.Select();
+	}
+
+	this.Deselect = function() {
+		nameEditor.Deselect();
+		valueEditor.Deselect();
 	}
 }
