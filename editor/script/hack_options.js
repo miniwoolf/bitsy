a3c2bc14246718bb354dc393a7713c4838c2ec33
miniwoolf <43096424+miniwoolf@users.noreply.github.com>
2021-09-16 21:15:22 -0700

    hid scrollbars in all browsers
    editor\index.html
    editor\script\hack_options.js
    editor\style\settingsToolStyle.css

--- editor/script/hack_options.js
+++ editor/script/hack_options.js
@@ -11,9 +11,11 @@ function toggleTransprites(e) {
 	transpritesHackState = (e.target.checked);
 	if (transpritesHackState) {
 		document.getElementById('transpritesOptions').style.display = 'inline-flex';
+		iconUtils.LoadIcon(document.getElementById("transpritesIcon"), "visibility");
 	}
 	else {
 		document.getElementById('transpritesOptions').style.display = 'none';
+		iconUtils.LoadIcon(document.getElementById("transpritesIcon"), "visibility_off");
 	}
 }
 
@@ -29,3 +31,28 @@ function resetHacksData() {
 	Store.set('transprites_setting', document.getElementById("transprites_setting").value); // save settings
 }
 
+/* var style = document.createElement("style");
+function hideScrollbar() {
+	style.innerHTML = `body::-webkit-scrollbar {display: none;}`;
+	document.head.appendChild(style);
+	document.getElementById("option_scrollbar").innerHTML = "hidden";
+	iconUtils.LoadIcon(document.getElementById("scrollbarIcon"), "visibility");
+}
+
+function showScrollbar() {
+	style.innerHTML = `body::-webkit-scrollbar {display: auto;}`;
+	document.head.appendChild(style);
+	document.getElementById("option_scrollbar").innerHTML = "visible";
+	iconUtils.LoadIcon(document.getElementById("scrollbarIcon"), "visibility_off");
+}
+
+var scrollbarShowing = true;
+function toggleScrollbar(e) {
+	scrollbarShowing = e.target.checked;
+	if (scrollbarShowing) {
+		showScrollbar();
+	}
+	else {
+		hideScrollbar();
+	}
+} */
